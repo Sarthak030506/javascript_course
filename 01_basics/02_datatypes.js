0cc2de0da4b8db0f71fdadc4bc5d7a64ce99be2c
Sarthak030506 <sarthakgodse03@gmail.com>
2025-06-12 07:41:15 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";
+//treat all Js code as newer version
+
+//alert("Hello") we are using node js not browser
+
+console.log()
+
+let name="Sarthak"
+let age= 19
+let isLoggedIn=true
+
+//number
+/*
+bigint
+string
+boolean
+null=>standalone value
+undefined 
+symbol=>unique
+object
+*/
+
+console.log(typeof null) //object
+console.log(typeof undefined)//undefine
+
+  
