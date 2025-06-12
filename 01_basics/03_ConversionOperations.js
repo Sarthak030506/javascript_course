f5fe82b47d8c85011c6dfe1f55fc73f15e383599
Sarthak030506 <sarthakgodse03@gmail.com>
2025-06-12 07:42:41 +0000

    conver

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,30 @@
+let score= "20"
+let final= "23abc"
+
+console.log(typeof(score))
+
+let ValueInNumber= Number(score)
+let FinalNumber= Number(final)
+
+console.log(typeof ValueInNumber)
+console.log(ValueInNumber)
+
+console.log(typeof FinalNumber)
+console.log(FinalNumber)
+
+console.log(null)
+console.log(undefined)
+
+/*
+"20"=>20
+"23abc"=>NaN
+""=>false
+"something"=>true
+*/
+
+let num=1
+
+console.log(++num)
+console.log(num++)
+
+
